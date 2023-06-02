90cb3d79a61dadc9fec8b32f6baa07b3ee5323fc
kyimoemin <kyimoemin0@gmail.com>
2023-06-02 20:54:05 +0630

    added Example 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import "./App.css";
 import { Example1 } from "./examples/Example1";
+import { Example2 } from "./examples/Example2";
 
 function App() {
-  return <Example1 />;
+  return <Example2 />;
 }
 
 export default App;
