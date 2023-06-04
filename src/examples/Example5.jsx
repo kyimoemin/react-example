1a771029924c46c23bc1a93a8e85870a0f4e9c1c
kyimoemin <kyimoemin0@gmail.com>
2023-06-04 20:50:30 +0630

    added useCallback

--- src/examples/Example5.jsx
+++ src/examples/Example5.jsx
@@ -1,21 +1,22 @@
 /**
- * use case of useEffect
+ * use case of useEffect and useCallback
  */
 
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 export function Example5() {
 
   const [count, setCount] = useState(0);
   const [color, setColor] = useState("#000");
-
-  useEffect(() => {
+  const changeColor = useCallback(() => {
     const isEven = !(count % 2);
     setColor(isEven ? "#f00" : "#0f0");
   }, [count]);
 
+  useEffect(changeColor, [count, changeColor]);
+
   return <div>
     <p style={{ color }}>{count}</p>
-    <button onClick={() => setCount(c => c + 1)}>increase</button>
+    <button onClick={() => { setCount(c => c + 1); }}>increase</button>
   </div>;
 }
