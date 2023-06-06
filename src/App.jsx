ce6bd11fa7f83403c98d82648ff3010a05fc28a7
kyimoemin <kyimoemin0@gmail.com>
2023-06-06 21:02:12 +0630

    added Example 9

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import "./App.css";
 import { Example7 } from "./examples/Example7";
 import { Example8 } from "./examples/Example8";
+import { Example9 } from "./examples/Example9";
 
 function App() {
-  return <Example8 />;
+  return <Example9 />;
 }
 
 export default App;
---
+++ src/examples/Example9.jsx
@@ -0,0 +1,102 @@
+import React, { memo, useState, useTransition } from 'react';
+
+function PostsTab() {
+  // Log once. The actual slowdown is inside SlowPost.
+  console.log('[ARTIFICIALLY SLOW] Rendering 500 <SlowPost />');
+
+  let items = [];
+  for (let i = 0; i < 500; i++) {
+    items.push(<SlowPost key={i} index={i} />);
+  }
+  return (
+    <ul className="items">
+      {items}
+    </ul>
+  );
+};
+
+function SlowPost({ index }) {
+  let startTime = performance.now();
+  while (performance.now() - startTime < 2) {
+    // Do nothing for 1 ms per item to emulate extremely slow code
+  }
+
+  return (
+    <li className="item">
+      Post #{index + 1}
+    </li>
+  );
+}
+
+
+export function Example9() {
+
+  const [isPending, startTransition] = useTransition();
+  const [tab, setTab] = useState('about');
+
+  function selectTab(nextTab) {
+    startTransition(() => {
+      setTab(nextTab);
+    });
+  }
+
+  return (
+    <>
+      <TabButton
+        isActive={tab === 'about'}
+        onClick={() => selectTab('about')}
+      >
+        About
+      </TabButton>
+      <TabButton
+        isActive={tab === 'posts'}
+        onClick={() => selectTab('posts')}
+      >
+        Posts (slow)
+      </TabButton>
+      <TabButton
+        isActive={tab === 'contact'}
+        onClick={() => selectTab('contact')}
+      >
+        Contact
+      </TabButton>
+      <hr />
+      {tab === 'about' && <AboutTab />}
+      {tab === 'posts' && <PostsTab />}
+      {tab === 'contact' && <ContactTab />}
+    </>
+  );
+
+}
+
+function TabButton({ children, isActive, onClick }) {
+  if (isActive) {
+    return <b>{children}</b>;
+  }
+  return (
+    <button onClick={() => {
+      onClick();
+    }}>
+      {children}
+    </button>
+  );
+}
+
+function ContactTab() {
+  return (
+    <>
+      <p>
+        You can find me online here:
+      </p>
+      <ul>
+        <li>admin@mysite.com</li>
+        <li>+123456789</li>
+      </ul>
+    </>
+  );
+}
+function AboutTab() {
+  return (
+    <p>Welcome to my profile!</p>
+  );
+}
